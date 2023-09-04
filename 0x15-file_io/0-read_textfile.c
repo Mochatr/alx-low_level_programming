87293cdab30b5fe162dc599b42fd30f94ce27f97
Mochatr <mohamedchatr8@gmail.com>
2023-09-04 14:23:37 -0700

    update

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Function
+ * @filename: parameter
+ * @letters: parameter
+ * Return:a value
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buff;
+	ssize_t wr, r, fd;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	buff = malloc(sizeof(char) * letters);
+	r = read(fd, buff, letters);
+	wr = write(STDOUT_FILENO, buff, r);
+
+	free(buff);
+	close (fd);
+	return (wr);
+}
