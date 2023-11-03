f5c45da90087035b2f353ce0034f8ed7339aec7a
Mochatr <mohamedchatr8@gmail.com>
2023-11-03 12:02:59 -0700

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hashTable->size = size;
 	hashTable->array = malloc(sizeof(hash_node_t *) * size);
 	if (hashTable->array == NULL)
+	{
+		free(hasTable);
 		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
 		hashTable->array[i] = NULL;
